57b7d4a5c6b537dfc39415d67ac6fb58e3e350af
Chansik Yun <chansigi@gmail.com>
2025-05-12 14:20:34 +0900

    .

---
+++ 4_변수3.cpp
@@ -0,0 +1,42 @@
+// 4_변수3.cpp
+#include <iostream>
+using namespace std;
+
+#if 0
+int main()
+{
+    double d1 = 3.14;
+    cout << d1 << endl;
+
+    int n1 = d1;
+    // C++은 실수 타입과 정수 타입간의 암묵적인 변환이 허용됩니다.
+    cout << n1 << endl;
+}
+#endif
+
+// 컴파일러마다 타입의 크기가 달라질 수 있습니다.
+// - 64bit: MSVC(long - 4바이트)
+//   64bit: GCC/Clang(long - 8바이트)
+
+int main()
+{
+    double d1{3.14};
+    cout << d1 << endl;
+
+    // 1. 실수와 정수 타입간의 암묵적인 변환을 허용하지 않습니다.
+    //  => 일관된 초기화를 사용할 때는 명시적인 캐스팅이 필요합니다.
+    int n1{static_cast<int>(d1)};
+    cout << n1 << endl;
+
+    cout << sizeof(int) << endl;
+    cout << sizeof(long) << endl;
+
+    // long n2 = 1000L;
+    // int n3 = n2;
+    // 정수 타입간의 암묵적인 변환도 위험합니다.
+
+    long n2{1000L};
+    int n3{static_cast<int>(n2)};
+    // 2. 표현 범위의 크기가 다른 정수간의 암묵적인 변환도 허용되지 않습니다.
+    //  - 명시적인 캐스팅이 필요합니다.
+}
