ce172197b7e80c1d024f0e407ad7bcb5e4f7c0cc
Chansik Yun <chansigi@gmail.com>
2025-05-12 14:23:18 +0900

    .

--- 4_변수3.cpp
+++ 4_변수3.cpp
@@ -14,10 +14,7 @@ int main()
 }
 #endif
 
-// 컴파일러마다 타입의 크기가 달라질 수 있습니다.
-// - 64bit: MSVC(long - 4바이트)
-//   64bit: GCC/Clang(long - 8바이트)
-
+#if 0
 int main()
 {
     double d1{3.14};
@@ -39,4 +36,26 @@ int main()
     int n3{static_cast<int>(n2)};
     // 2. 표현 범위의 크기가 다른 정수간의 암묵적인 변환도 허용되지 않습니다.
     //  - 명시적인 캐스팅이 필요합니다.
+}
+#endif
+
+// 컴파일러마다 타입의 크기가 달라질 수 있습니다.
+// - 64bit: MSVC(long - 4바이트)
+//   64bit: GCC/Clang(long - 8바이트)
+
+// C/C++ 표준에서 타입의 크기가 정해져 있는 것은 오직 하나의 타입입니다.
+// => char 타입이 1바이트 입니다.
+// => 컴파일러와 플랫폼에 따라서 정수 타입의 크기는 달라질 수 있습니다.
+
+#include <stdint.h> // C99
+
+int main()
+{
+    // int, char, short, long, long long 을 직접 사용하는 것은
+    // 좋지 않습니다.
+    int8_t a;
+    int16_t b;
+    int32_t c;
+
+    uint8_t a2;
 }
