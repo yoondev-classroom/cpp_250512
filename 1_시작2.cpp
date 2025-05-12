f13dc97ed0cbc20aac07f5c5815ce2ad757b8601
Chansik Yun <chansigi@gmail.com>
2025-05-12 11:19:24 +0900

    .

---
+++ 1_시작2.cpp
@@ -0,0 +1,20 @@
+// 1_시작2.cpp
+#if 0
+#include <stdio.h> // C언어 헤더
+
+int main()
+{
+    printf("Hello, C\n"); // C언어 함수
+}
+#endif
+
+// C++도 표준 입출력 라이브러리가 있습니다.
+#include <iostream>
+// C++의 표준 입출력 라이브러리 헤더 파일입니다.
+// C++의 라이브러리의 헤더 파일은 헤더 파일 확장자가 없는
+// 경우가 많습니다.
+
+int main()
+{
+    std::cout << "Hello, C++" << std::endl;
+}
