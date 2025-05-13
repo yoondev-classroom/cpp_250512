ea8d80a9d86b0843239059d8047fe20a4b24a593
Chansik Yun <chansigi@gmail.com>
2025-05-13 15:37:31 +0900

    .

--- 13_stack3.cpp
+++ 13_stack3.cpp
@@ -9,15 +9,29 @@ using namespace std;
 
 // 해결방법
 // : C++에서는 데이터와 데이터를 조작하는 함수를
-//   캡슐화 할 수 있습니다.
+//   캡슐화(상태 + 행위) 할 수 있습니다.
 
-struct Stack
+// 객체의 상태는 외부에서 함부로 조작되는 것이 위험합니다.
+// => 객체의 상태는 객체가 제공하는 메소드(멤버 함수)를 통해서만 접근 가능해야 합니다.
+// => 정보 은닉(Information Hiding)
+
+// public: 외부에서 접근이 가능합니다.
+// private: 외부에서 접근이 불가능합니다.
+
+// struct는 기본 접근 지정자가 public 입니다.
+// class는 기본 접근 지정자가 private 입니다.
+// => C++ 에서 객체를 캡슐화할 때, 일반적으로 class를 사용하는 것이 좋습니다.
+
+// struct Stack
+class Stack
 {
-    // 상태
+    // private:
+    // 상태 - 멤버 데이터
     int buff[10];
     int top;
 
-    // 상태를 조작하는 함수
+public:
+    // 상태를 조작하는 함수 - 멤버 함수 / 메소드
     void Init()
     {
         top = 0;
@@ -37,9 +51,14 @@ struct Stack
 int main()
 {
     Stack s;
+    // 상태와 행위를 가지는 변수
+    // => 객체(object)
+    // s.top = -1; // !!!
+
     s.Init();
 
     s.Push(10);
+
     s.Push(20);
     s.Push(30);
 
