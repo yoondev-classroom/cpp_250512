fe7dd0e80767745fc0be7cba2f30a682e627d3a5
Chansik Yun <chansigi@gmail.com>
2025-05-15 15:16:46 +0900

    .

--- 25_다형성8.cpp
+++ 25_다형성8.cpp
@@ -18,6 +18,9 @@ using namespace std;
 // 4) 자식이 오버라이딩 하는 모든 멤버 함수는 반드시 가상 함수 이어야 한다.
 // 5) 부모 클래스의 소멸자는 반드시 가상이어야 합니다.
 
+// 6) 수정에는 닫혀 있고, 확장에는 열려 있어야 한다. => OCP(Open Close Principle)
+//   "새로운 기능이 추가되어도, 기존 코드는 수정되면 안된다."
+
 class Shape
 {
 public:
@@ -29,12 +32,21 @@ class Rect : public Shape
 {
 public:
     void Draw() const override { cout << "Draw Rect" << endl; }
+    ~Rect() override { cout << "~Rect()" << endl; }
 };
 
 class Circle : public Shape
 {
 public:
     void Draw() const override { cout << "Draw Circle" << endl; }
+    ~Circle() override { cout << "~Circle()" << endl; }
+};
+
+class Triangle : public Shape
+{
+public:
+    void Draw() const override { cout << "Draw Triangle" << endl; }
+    ~Triangle() override { cout << "~Triangle()" << endl; }
 };
 
 int main()
@@ -54,11 +66,21 @@ int main()
         {
             shapes.push_back(new Circle);
         }
+        else if (cmd == 3)
+        {
+            shapes.push_back(new Triangle);
+        }
+
         else if (cmd == 9)
         {
             for (Shape *e : shapes)
             {
                 e->Draw();
+                // e가 Rect -> Rect::Draw
+                //   Circle -> Circle::Draw
+                //  Triangle -> Triangle::Draw
+                // "다형성, Polymorphism"
+                // - 다형성은 OCP를 만족합니다.
             }
         }
         else if (cmd == 0)
@@ -80,3 +102,16 @@ int main()
     vector<Circle *> circles;
 }
 #endif
+
+// UNIX / Linux
+//   : VFS(virtual file system)
+
+// - 모든 것은 파일입니다.
+//   일반 파일
+//   소켓 파일
+//   디바이스 특수 파일
+
+// write()
+//    일반 파일 -> 파일의 내용을 변경합니다.
+//    소켓 파일 -> 데이터를 전송합니다.
+//    디바이스 파일 -> 장치에 명령을 전송합니다.
