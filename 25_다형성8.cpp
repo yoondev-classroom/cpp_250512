cabf38445497702cdb21c878cd062869ec04ddf2
Chansik Yun <chansigi@gmail.com>
2025-05-15 14:52:41 +0900

    .

---
+++ 25_다형성8.cpp
@@ -0,0 +1,82 @@
+// 25_다형성8.cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 추상화
+// 1) 각 도형의 클래스를 설계
+// 2) 부모 클래스 도입
+//   => 동종을 보관하는 컨테이너
+//   => Upcasting: 부모의 포인터/참조를 통해 자식 객체를 참조
+
+// 3) 자식의 공통의 기능을 부모의 포인터/참조를 통해 이용하기 위해서는
+//   반드시 해당 특징이 부모 클래스로부터 비롯되어야 한다.
+//   => LSP(Liskov Substisution Principle, 리스코프 치환 원칙)
+//     : 자식 클래스는 부모 클래스로 대체할 수 있어야 한다.
+//       "자식의 공통의 기능을 부모로부터 비롯되어야 한다"
+
+// 4) 자식이 오버라이딩 하는 모든 멤버 함수는 반드시 가상 함수 이어야 한다.
+// 5) 부모 클래스의 소멸자는 반드시 가상이어야 합니다.
+
+class Shape
+{
+public:
+    virtual void Draw() const { cout << "Draw Shape" << endl; }
+    virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+    void Draw() const override { cout << "Draw Rect" << endl; }
+};
+
+class Circle : public Shape
+{
+public:
+    void Draw() const override { cout << "Draw Circle" << endl; }
+};
+
+int main()
+{
+    vector<Shape *> shapes;
+
+    while (1)
+    {
+        int cmd;
+        cin >> cmd;
+
+        if (cmd == 1)
+        {
+            shapes.push_back(new Rect);
+        }
+        else if (cmd == 2)
+        {
+            shapes.push_back(new Circle);
+        }
+        else if (cmd == 9)
+        {
+            for (Shape *e : shapes)
+            {
+                e->Draw();
+            }
+        }
+        else if (cmd == 0)
+        {
+            break;
+        }
+    }
+
+    for (Shape *e : shapes)
+    {
+        delete e;
+    }
+}
+
+#if 0
+int main()
+{
+    vector<Rect *> rects;
+    vector<Circle *> circles;
+}
+#endif
